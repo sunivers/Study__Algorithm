38dfa2703b7e3c8b3aa19b5c626d45b0bd82dae6
soyoung <soyoungp02@gmail.com>
2020-06-05 23:39:14 +0900

    ✅add practice solution

---
+++ Section_01/3. k번째 큰 수/aa.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin=open('in1.txt', 'rt')
+
+n, k=map(int, input().split())
+a=list(map(int, input().split()))
+res=set()
+
+for i in range(n):
+  for j in range(i+1, n):
+    for l in range(j+1, n):
+      res.add(a[i]+a[j]+a[l])
+
+res=list(res)
+res.sort(reverse=True)
+print(res[k-1])
