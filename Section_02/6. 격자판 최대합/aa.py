31cf6b6d6b2add23567e57c917a8516f5cd857d6
soyoung <soyoungp02@gmail.com>
2020-07-29 23:12:32 +0900

    ✅ add practice solution 2-6

---
+++ Section_02/6. 격자판 최대합/aa.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('in5.txt', 'rt')
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+largest=-2147000000 # 대충 가장 작은 정수
+for i in range(n):
+  sum1=sum2=0
+  for j in range(n):
+    sum1 += a[i][j]
+    sum2 += a[j][i]
+  if sum1 > largest:
+    largest = sum1
+  if sum2 > largest:
+    largest = sum2
+
+sum1=sum2=0
+for i in range(n):
+  sum1 += a[i][i]
+  sum2 += a[i][n-1-i]
+if sum1 > largest:
+  largest = sum1
+if sum2 > largest:
+  largest = sum2
+
+print(largest)
