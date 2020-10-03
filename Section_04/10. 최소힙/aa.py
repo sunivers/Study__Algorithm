01aba4bf736f6da5a372eefc92982a878a565e3f
soyoung <soyoungp02@gmail.com>
2020-10-03 22:01:22 +0900

    ✅ add practice solution 4-10

---
+++ Section_04/10. 최소힙/aa.py
@@ -0,0 +1,18 @@
+import sys
+import heapq as hq
+sys.stdin = open('in3.txt', 'r')
+
+a = []
+while True:
+  n = int(input())
+  if n == -1:
+    break
+  if n == 0:
+    if len(a) == 0:
+      print(-1)
+    else:
+      print(hq.heappop(a))
+  else:
+    hq.heappush(a, n)
+  
+  
