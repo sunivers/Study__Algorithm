47353bed7d46a914ca047d60e571cf88349876b5
soyoung <soyoungp02@gmail.com>
2020-11-01 21:34:26 +0900

    ♻️ refactor practice solution 5-8

--- Section_05/8. 순열 구하기/aa.py
+++ Section_05/8. 순열 구하기/aa.py
@@ -2,26 +2,25 @@ import sys
 sys.stdin = open("in5.txt", "r")
 
 n, m = map(int, input().split())
-a = [0] * m
+res = [0] * m
 chk = [0] * (n + 1)
 cnt = 0
 
-def DFS(i, L):
-  global a, chk, cnt
+def DFS(L):
+  global res, chk, cnt
   if L == m:
-    for x in a:
+    for x in res:
       print(x, end=' ')
     print()
     cnt += 1
-    chk[i] = 0
     return
   else:
-    chk[i] = 1
-    for x in range(1, n + 1):
-      if chk[x] == 0:
-        a[L] = x
-        DFS(x, L + 1)
-    chk[i] = 0
+    for i in range(1, n + 1):
+      if chk[i] == 0:
+        chk[i] = 1
+        res[L] = i
+        DFS(L + 1)
+        chk[i] = 0
 
-DFS(0, 0)
+DFS(0)
 print(cnt)
